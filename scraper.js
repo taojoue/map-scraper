67d4fe6a4244bcbd4f39f8726c6d7823789e4a0e
taojoue <taojouet@gmail.com>
2022-05-16 11:57:46 +0200

    add script for all images

--- scraper.js
+++ scraper.js
@@ -1,14 +1,21 @@
-import sharp from "sharp/lib/sharp";
-
 function index() {
-    var Scraper = require("image-scraper");
+    const { exec } = require("child_process");
+    const sharp =  require('sharp');
 
-    var scraper = new Scraper("https://apod.nasa.gov/apod/astropix.html");
+    for (let i = 9; i <= 54; i++) {
+        for (let n = 0; n <= 63; n++) {
+            let url = "https://tiles.telegeography.com/maps/submarine-cable-map-2015/6/" + n.toString() + "/" + i.toString() + ".png";
+            let name = "6-" + n.toString() + "-" + i.toString();
+            exec("curl -s " + url + " -o ./images/" + name + ".jpg");
+        }
+    }
+    sharp('./images/6-0-9.png')
+        .composite([{
+            input: './images/6-0-9.png'
+        }])
+        .toFile('output.jpg', function (err) {
+            console.log("error: ", err)
+        });
 
-    scraper.address = "http://www.npmjs.org";
-
-    scraper.scrape(function(image) { 
-        image.save();
-    });
 }
-module.exports(index);
+module.exports = index();
